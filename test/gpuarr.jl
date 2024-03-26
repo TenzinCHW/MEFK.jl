d559091ebcba2fb4f8b241ae4e0c18e01b989cc9
Tenzin Chan <tenzin_chan@mymail.sutd.edu.sg>
2024-03-26 23:00:26 +0800

    Added GPU tests. K version doesn't work for Metal, some error occurs.

---
+++ test/gpuarr.jl
@@ -0,0 +1,28 @@
+import MEFK: MEFMPNK, dynamics, make_inds_winds
+import Flux, Test, CUDA
+
+
+n = 4
+N = 2
+array_cast = CUDA.cu
+
+inds, winds = make_inds_winds(n, N)
+model = MEFMPNK(n, N, inds; array_cast=array_cast)
+model = MEFMPNK(n, N; array_cast=array_cast)
+optim = Flux.setup(Flux.Adam(0.1), model)
+x = [[1, 0, 1, 0] [0, 1, 0, 1]]' .|> Int8
+
+for i in 1:100
+    loss, grads = model(x, i==1; reset_grad=true)
+    Flux.update!(optim, model, grads)
+end
+
+pred = dynamics(model, x; array_cast=array_cast)
+
+W1 = Float32[-12.1044, -12.3351, -24.5112, -11.0210]
+W2 = Float32[-3.1486, 15.7777,  3.5976, 18.6813,  0.5786,  6.8495] |> array_cast
+model.W[1] = W1
+model.W[2] = W2
+gt = dynamics(model, x; array_cast=array_cast)
+Test.@test all(pred == gt == y)
+
---
+++ test/gpufull.jl
@@ -0,0 +1,32 @@
+import MEFK, Flux, LinearAlgebra, CUDA
+
+array_cast = CUDA.cu
+n = 4
+model = MEFK.MEF3T(n; array_cast=array_cast)
+optim = Flux.setup(Flux.Adam(0.1), model)
+x = [[1, 0, 1, 0] [0, 1, 0, 1]]' .|> Int8
+counts = ones(2)
+
+for i in 1:100
+    loss, grads = model(x, counts; reset_grad=true)
+    Flux.update!(optim, model, grads)
+end
+
+en = MEFK.energy(model, x)
+x_hat = MEFK.dynamics(model, x) |> Array
+@assert all(x_hat == x)
+
+
+# test partial iteration feature and no symmetrization of gradients
+model = MEFK.MEF3T(4; array_cast=array_cast)
+optim = Flux.setup(Flux.Adam(0.1), model)
+
+for i in 1:100
+    loss, grads = model(x, counts; iterate_nodes=3:4, symmetrize_grad=false, reset_grad=true)
+    Flux.update!(optim, model, grads)
+end
+
+xp = [[1, 0, 0, 1] [0, 1, 1, 0]]' .|> Int8 |> array_cast
+x_hat = MEFK.dynamics(model, xp; iterate_nodes=3:4) |> Array
+@assert all(x_hat == x)
+
